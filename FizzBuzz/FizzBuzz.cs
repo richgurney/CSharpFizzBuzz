c9bbfdb77dc1ac01870bdbba0665719ac009693d
Richard Gurney <richgurney1987@gmail.com>
2019-03-14 20:50:57 +0000

    Intial commit

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FizzBuzz
+{
+    public class FizzBuzz
+    {
+        public List<string> FizzBuzzList;
+
+        public FizzBuzz(int StartOfRange, int EndOfRange)
+        {
+            FizzBuzzList = new List<string>();
+            FizzBuzzIterator(GenerateRange(StartOfRange, EndOfRange));
+        }
+
+        public bool DivisibleBy(int num1, int num2)
+        {
+            return num1 % num2 == 0;
+        }
+
+        public IEnumerable<int> GenerateRange(int StartOfRange, int EndOfRange)
+        {
+            IEnumerable<int> range = Enumerable.Range(StartOfRange, EndOfRange);
+
+            return range;
+        }
+
+        public void FizzBuzzIterator(IEnumerable<int> range)
+        {
+            foreach (var num in range)
+            {
+                if (DivisibleBy(num, 15))
+                {
+                    FizzBuzzList.Add("FizzBuzz");
+                }
+                else if (DivisibleBy(num, 5))
+                {
+                    FizzBuzzList.Add("Buzz");
+                }
+                else if (DivisibleBy(num, 3))
+                {
+                    FizzBuzzList.Add("Fizz");
+                }
+                else
+                {
+                    FizzBuzzList.Add(num.ToString());
+                }
+            }
+        }
+    }
+}
---
+++ FizzBuzz/tests/FizzBuzzTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace FizzBuzz
+{
+    [TestFixture()]
+    public class FizzBuzzTest
+    {
+        FizzBuzz fizzbuzz = new FizzBuzz(1, 15);
+
+        [Test()]
+        public void TestDivisibleBy()
+        {
+            Assert.AreEqual(true, fizzbuzz.DivisibleBy(6, 3));
+            Assert.AreEqual(false, fizzbuzz.DivisibleBy(5, 3));
+
+        }
+
+        [Test()]
+        public void TestFizzBuzzIterator()
+        {
+            Assert.AreEqual("Fizz", fizzbuzz.FizzBuzzList[2]);
+            Assert.AreEqual("Buzz", fizzbuzz.FizzBuzzList[4]);
+            Assert.AreEqual("FizzBuzz", fizzbuzz.FizzBuzzList[14]);
+        }
+    }
+}
